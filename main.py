6d958393eda5ef7486b33a39ae48ab9c9e515a06
IsmaGold67 <ismaelroberto67@gmail.com>
2024-11-27 18:28:56 -0400

    V1.0.2 - adiciona rota 1 e rota 2

--- main.py
+++ main.py
@@ -11,5 +11,16 @@ def raiz():
 def saudacoes(nome):
     return f'Olá, {nome}'
 
+# Feature código aula 03
+@app_isma.route('/rota1')
+def rota1():
+    return 'Olá Usuário!!'
+
+@app_isma.route('/rota2')
+def rota2():
+    resposta = "<h3> Essa é outra página da rota 2 </h3>"
+    return resposta
+
+# Feature código aula 02
 if __name__ == "__main__":
     app_isma.run()
