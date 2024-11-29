47503d72d4a44d96cc70ca936410ea6a1ab497e6
IsmaGold67 <ismaelroberto67@gmail.com>
2024-11-29 01:38:08 -0400

    dinimui quantidade de linhas da main

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template
 
-app_isma = Flask(__name__, template_folder='t_templates')
+app_isma = Flask(__name__)
 
 @app_isma.route("/index")
 def indice():
-    return render_template("t_index.html")
+    return render_template("index.html")
 
 @app_isma.route("/contato")
 def contato():
-    return render_template("t_contato.html")
+    return render_template("contato.html")
 
 @app_isma.route("/usuario", defaults={"nome_usuario":"usuário?","nome_profissao":""})
 def usuarios(nome_usuario, nome_profissao):
     dados_usu = {"profissao":nome_profissao, "disciplina":"Desenvolvimento WEB III"}
-    return render_template("t_usuario.html", nome=nome_usuario, dados = dados_usu)
+    return render_template("usuario.html", nome=nome_usuario, dados = dados_usu)
 
 if __name__ == "__main__":
     app_isma.run()
