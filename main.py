7658ee3292153e93b4c08e4ca43c93e0bf05eed3
IsmaGold67 <ismaelroberto67@gmail.com>
2024-12-02 15:43:33 -0400

    adiciona config javascript

--- main.py
+++ main.py
@@ -2,22 +2,13 @@ from flask import Flask, render_template
 
 app_isma = Flask(__name__)
 
-@app_isma.route("/")
+@app_isma.route("/cadastro")
 def homepage():
-    return render_template("homepage.html")
+    return render_template("cadastro.html")
 
-@app_isma.route("/index")
+@app_isma.route("/login")
 def indice():
-    return render_template("index.html")
-
-@app_isma.route("/contato")
-def contato():
-    return render_template("contato.html")
-
-@app_isma.route("/usuario", defaults={"nome_usuario":"usuário?","nome_profissao":""})
-def usuarios(nome_usuario, nome_profissao):
-    dados_usu = {"profissao":nome_profissao, "disciplina":"Desenvolvimento WEB III"}
-    return render_template("usuario.html", nome=nome_usuario, dados = dados_usu)
+    return render_template("login.html")
 
 if __name__ == "__main__":
-    app_isma.run()
+    app_isma.run(debug=True)
