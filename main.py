fc15ecfeb1bde10716813fddbbd596f45007bcd6
IsmaGold67 <ismaelroberto67@gmail.com>
2024-12-02 16:15:20 -0400

    adiciona rota homepage e cria html contatos

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template
 app_isma = Flask(__name__)
 
 @app_isma.route("/cadastro")
-def homepage():
+def cadastro():
     return render_template("cadastro.html")
 
 @app_isma.route("/login")
 def indice():
     return render_template("login.html")
+
+@app_isma.route("/homepage")
+def homepage():
+    return render_template("homepage.html")
 
 if __name__ == "__main__":
     app_isma.run(debug=True)
